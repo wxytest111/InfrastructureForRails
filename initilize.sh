a6493edc176d311dad1668c8a44ae7ac25e762bf
root <root@iZ284subyk2Z.(none)>
2015-02-13 01:00:22 +0800

    can install rvm

--- initilize.sh
+++ initilize.sh
@@ -3,9 +3,10 @@
 set -e
 
 yum update -y
-yum install curl
-yum install gcc-c++ patch readline readline-devel zlib zlib-devel
-yum install libyaml-devel libffi-devel openssl-devel make 
-yum install bzip2 autoconf automake libtool bison iconv-devel
+yum install -y curl
+yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel
+yum install -y libyaml-devel libffi-devel openssl-devel make 
+yum install -y bzip2 autoconf automake libtool bison iconv-devel
+curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 curl -L get.rvm.io | bash -s stable
-
+source /etc/profile.d/rvm.sh
