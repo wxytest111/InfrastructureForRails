f839be2d4e6b4c486cb832e328d4bae0f5d9a139
xinyu <30540047@qq.com>
2015-04-01 23:04:40 +0800

    mysql

--- initilize.sh
+++ initilize.sh
@@ -7,11 +7,14 @@ yum install -y curl
 yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel
 yum install -y libyaml-devel libffi-devel openssl-devel make 
 yum install -y nodejs bzip2 autoconf automake libtool bison iconv-devel
+
+yum install -y mysql mysql-server
+
 curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 curl -L get.rvm.io | bash -s stable
 `source /etc/profile.d/rvm.sh`
-rvm install 2.1.2
-rvm use 2.1.2
-rvm --default 2.1.2
+rvm install 2.1.5
+rvm use 2.1.5
+rvm --default 2.1.5
 gem install rake
 gem install rails --version 4.2.0
